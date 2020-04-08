1cd4893638bb44d71dfd6fcba594104ccedf6fa4
Kimminsu <alstn2468_@naver.com>
2020-04-08 14:27:16 +0900

    2020년 4월 8일 컴퓨터 그래픽스 3주차 과제 비어있는 주전자 코드 업로드

---
+++ 2020_04_08/2_14_DrawWireTeapot.cpp
@@ -0,0 +1,31 @@
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+void drawWireTeapot()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.4f, 0.8f, 0.6f);
+
+	glutWireTeapot(0.5f);
+
+	glFlush();
+}
+
+int main(int argc, char** argv) 
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("3주차 과제 2 - 주전자 (2)");
+
+	glMatrixMode( GL_MODELVIEW );
+	glLoadIdentity();
+	gluLookAt(0, 0, 0, 5, 5, 15, 0, 1, 0);
+
+	glutDisplayFunc(drawWireTeapot);
+	glutMainLoop();
+
+	return 0;
+}
