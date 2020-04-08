8ee465805c383eb132c99a9346c5a73cc9a0ef94
Kimminsu <alstn2468_@naver.com>
2020-04-08 14:57:47 +0900

    2020년 4월 8일 컴퓨터 그래픽스 과제 2 비어있는 정8면체 코드 업로드

---
+++ 2020_04_08/2_18_DrawWireOctahedron.cpp
@@ -0,0 +1,32 @@
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+void drawWireOctahedron()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.4f, 0.8f, 0.6f);
+	glScalef(0.3,0.3,0.3);
+
+	glutWireOctahedron();
+
+	glFlush();
+}
+
+int main(int argc, char** argv) 
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("3주차 과제 2 - 정8면체 (2)");
+
+	glMatrixMode( GL_MODELVIEW );
+	glLoadIdentity();
+	gluLookAt(0, 0, 0, 5, 5, 15, 0, 1, 0);
+
+	glutDisplayFunc(drawWireOctahedron);
+	glutMainLoop();
+
+	return 0;
+}
